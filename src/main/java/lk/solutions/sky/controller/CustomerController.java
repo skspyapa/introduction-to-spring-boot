4e9344d6110b2628b4fa20fbffa1c22c9fc19909
skspyapa <skspyapa@gmail.com>
2025-08-19 02:38:08 +0530

    fix compile error in controller

--- src/main/java/lk/solutions/sky/controller/CustomerController.java
+++ src/main/java/lk/solutions/sky/controller/CustomerController.java
@@ -1,9 +1,10 @@
 package lk.solutions.sky.controller;
 
+import lk.solutions.sky.entity.Customer;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
 
-    public Customer
+    public Customer getCustomer() {return new Customer();}
 }
